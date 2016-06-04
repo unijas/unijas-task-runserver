eda5e779e1f79a8d0973407e71430909a9caa2d5
Siggi Duenkel <chaosprinz76@googlemail.com>
2016-06-04 12:30:35 +0200

    made it configurable

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,15 @@
 const gulp = require('gulp')
 const Nodemon = require('gulp-nodemon')
 const Join = require('path').join
+const Helper = require('unijas-task-helper')
 
+let configFile = Join(process.cwd(), 'config', 'build.conf.json')
+let cfg = Helper.readConfigFile(__dirname, configFile).runserver
 gulp.task('runserver', function () {
-  let stream = Nodemon({
-    script: Join(process.cwd(), 'index.js'),
-    watch: [Join(process.cwd(), 'api'), Join(process.cwd(), 'index.js')],
-    ext: 'js'
-  })
-  return stream
+  let nodemonCfg = cfg.nodemon
+  nodemonCfg.script = Helper.buildPathes(nodemonCfg.script)
+  nodemonCfg.watch = Helper.buildPathes(nodemonCfg.watch)
+  return Nodemon(nodemonCfg)
 })
 
 module.exports = gulp.tasks
